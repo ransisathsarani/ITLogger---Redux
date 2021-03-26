7c7186ad5fd6a58f578f874c48010d17f38fd156
sathsaranir1996 <38213964+sathsaranir1996@users.noreply.github.com>
2021-03-26 10:13:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Fragment, useEffect} from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    //Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar/>
+      <div className="container">
+        <Logs/>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Perloader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Perloader = () => {
+    return (
+        <div className="progress blue lighten-4">
+            <div className="indeterminate blue">
+                </div> 
+        </div>
+    )
+}
+
+export default Perloader;
