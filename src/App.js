c5c36075596b54b97236a6c215e53cbbe6eb2a36
sathsaranir1996 <38213964+sathsaranir1996@users.noreply.github.com>
2021-03-29 13:22:33 +0530

    Added components and UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, {Fragment, useEffect} from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 
 import 'materialize-css/dist/css/materialize.min.css';
@@ -16,6 +21,11 @@ const App = () => {
     <Fragment>
       <SearchBar/>
       <div className="container">
+        <AddBtn/>
+        <AddLogModal/>
+        <EditLogModal/>
+        <AddTechModal/>
+        <TechListModal/>
         <Logs/>
       </div>
     </Fragment>
